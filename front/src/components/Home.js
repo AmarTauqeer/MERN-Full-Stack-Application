9966a113474a7af01848de85972441e40a8f4e9a
Amar Tauqeer <amar.tauqeer@gmail.com>
2020-08-11 14:49:36 +0200

    add front folder

---
+++ front/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+
+    render() {
+        return (
+            <div className="container border border-secondary">
+                <div className="container w-50" style={{ marginTop: 20, marginBottom: 20 }}>
+                    <h5 align="center">Welcome to Hitech Order Management System.
+            </h5>
+                </div>
+            </div>
+        )
+    }
+
+}
